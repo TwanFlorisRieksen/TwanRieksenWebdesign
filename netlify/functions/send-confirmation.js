5979fc82406505e042a58f62a3015e4ba7a56ffc
TwanFlorisRieksen <twan.rieksen@gmail.com>
2026-01-04 19:01:06 +0100

    Update send-confirmation.js

--- netlify/functions/send-confirmation.js
+++ netlify/functions/send-confirmation.js
@@ -21,7 +21,7 @@ exports.handler = async (event) => {
 
     // Tot je domein verified is: gebruik onboarding@resend.dev als from
     // en zet jouw eigen domein in reply_to.
-    const from = "Twan Rieksen Webdesign <no-reply@twanrieksenwebdesign.nl>";
+    const from = "Twan Rieksen Webdesign <onboarding@resend.dev>";
     const reply_to = "twan.rieksen@gmail.com";
 
     const subject = "Bevestiging: uw bericht is goed ontvangen";
